e5dec467f4b06473808e5fd3daf9646e37ca1379
RoPro99 <rohankodag499@gmail.com>
2022-06-03 23:10:49 +0530

    Opearators

---
+++ Day-2/Operator.js
@@ -0,0 +1,62 @@
+//Arthmatic operator
+// const a = 10;
+// const b= 4;
+// let result = 0;
+// //Addition 
+//result = a + b;
+
+//Substraction
+//result = a- b;
+
+
+//Multiplication
+//result = a*b;
+
+//Divivsion
+//result = a/b;
+
+//Modulo
+//result = a%b;
+
+//Increment
+//result++;
+
+//decrement
+// result --;
+
+// console.log(result)
+
+
+//Comparision Operator
+const a= 5;
+const b= 10;
+
+//is equal
+console.log(a == b);
+//is greater 
+console.log(a>b);
+//greter then equal
+console.log(a>=b);
+//less then equal
+console.log(a<=b);
+//is not equal
+console.log(a!=b);
+//Strict Euality-->compare value and data type
+console.log(a===b);
+//Strict Not Quality
+console.log(a!==b);
+
+//Logical Operator
+//AND-->all operand are true
+console.log(true && true);
+//OR-->any one is true
+console.log(true || false);
+//Not-->opposite
+console.log(!true);
+
+//Assignmnet
+//equal
+let r=8;
+r += r;//r=r+8
+console.log(r);
+
