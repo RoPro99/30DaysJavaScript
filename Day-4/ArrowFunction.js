7c6eec2e2a64c73d0596139fea34417efe0e7a2e
RoPro99 <rohankodag499@gmail.com>
2022-06-06 20:55:30 +0530

    Functions

---
+++ Day-4/ArrowFunction.js
@@ -0,0 +1,15 @@
+//Arrow Syntax
+//declaration
+const square = (num)=>{
+    return num*num;
+}
+
+const add= (num1)=>num1+num1; 
+
+//Calling
+
+const result = square(5);
+const result2 = add(5);
+
+console.log(result);
+console.log(result2);
---
+++ Day-4/Function.js
@@ -0,0 +1,39 @@
+///Function is a block of code to perfrom multiple task.
+//Function Declaration
+function square(num){
+    return num*num;
+}
+
+
+//Function Calling
+const result = square(9);
+console.log(result);
+
+//Methods to create function
+
+//method - 1
+function name(params){
+    //code
+}
+
+//method - 2
+const name2 = function(params){
+    //code
+}
+//method - 3 arrow
+const name1 = (paramsa)=>{
+    //code
+    console.log('Hey Whats Uppp');
+}
+//invoke method 3 
+name1(2);
+
+//fonstion return
+
+function add(a,b){
+    //code
+    return a+b;
+
+}
+const integer= add(5,5);
+console.log(integer);
